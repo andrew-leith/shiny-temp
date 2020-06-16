d2ae351a4444e4847dbcc1a61474d08e7cc89e8c
andrew-leith <andrew-leith@users.noreply.github.com>
2020-06-16 08:59:38 -0400

    removing elements

--- ui_tester.R
+++ ui_tester.R
@@ -591,22 +591,8 @@ iDEPversion,
   
     # sidebar of DEG3 --------------------------------------------------------------------------------
       sidebarPanel(
-        h5("Identifying Differential Expressed Genes (DEGs). See next tab for details."),
+        h5("Cook's Distance Plot Parameters."),
         ,conditionalPanel( "input.dataFileFormat == 2", h5("Using the limma package")        )        
-        ,fluidRow(
-          column(5,numericInput("limmaPval",  # FDR cutoff
-                 label = h5("FDR cutoff"), 
-                 value = 0.1,
-                 min   = 1e-5,
-                 max   = 1,
-                 step  =.05)  )
-          ,column(7, numericInput("limmaFC",   # fold change cutoff
-                  label = h5("Min fold change"),
-                  value = 2,
-                  min   = 1,
-                  max   = 100,
-                  step  = 0.5) )
-        ) # fluidRow
         ,tags$style(type='text/css', "#limmaPval { width:100%;   margin-top:-12px}")
         ,tags$style(type='text/css', "#limmaFC { width:100%;   margin-top:-12px}")
         ,actionButton("modelAndComparisons", "Select factors & comparisons")
@@ -633,8 +619,10 @@ iDEPversion,
             
       mainPanel(
         plotOutput('sigGeneStats')
-        ,br(),br(),
-        h4("Numbers of differentially expressed genes for all comparisons.
+        ,br(),br()
+        ,h4("Cook Plot")
+        ,plot(c(1, 2, 3, 4), c(1, 2, 3, 4))
+        ,h4("Numbers of differentially expressed genes for all comparisons.
            \"B-A\" means B vs. A. Interaction terms start with \"I:\" ")
         ,tableOutput('sigGeneStatsTable')
     
